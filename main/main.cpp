2e7c3f005a6e879b69e316b8b31c5954d6644a49
Alina Mansurova <TheDolp@outlook.com>
2022-12-05 21:53:14 +0300

    doing work

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,44 @@
-﻿#include <SFML/Graphics.hpp>
-#include "Plane.h";
+﻿#include <iostream>
+#include <vector>
+#include <SFML/Graphics.hpp>
 
-using namespace sf;
+class Item {
+public:
+	std::string name;
+	int article;
+	int price;
+	int discount;
+	int last_day;
+	int duration;
+	double interest;
+};
+
+class Package : public Item {
+public:
+	int count;
+};
+
+class PackageList {
+public:
+	std::vector <Package> list;
+};
+
+class Case {
+public:
+	std::vector <PackageList> shelves;
+	std::vector <int> max_count;
+};
+
+class Market {
+public:
+	Market(int item_count, PackageList& basic_shelf) {
+		customer_count = 100;
+	}
+
+	int customer_count;
+	PackageList shelf;
+};
 
 int main() {
-	Plane* plane = new Plane();
-	//plane do something
-	delete plane;
+
 }
