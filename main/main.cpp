f690391978d183bb5287b26c72cb0d61c336b65e
Иван Белецкий <iabeletsky@gmail.com>
2022-12-12 19:52:50 +0300

    camelStyle

--- main/main.cpp
+++ main/main.cpp
@@ -78,43 +78,43 @@ private:
 class Market {
 public:
 	Market(std::vector<Package>& basic_shelf) {
-		customer_count = 100;
+		customer_count_ = 100;
 		for (Package package : basic_shelf) {
-			shelf.push_back(package);
-			max_count.push_back(customer_count * package.getInterest() * package.getDuration());
+			shelf_.push_back(package);
+			max_count_.push_back(customer_count_ * package.getInterest() * package.getDuration());
 		}
 	}
 
-	void GetProducts(std::vector<Package>& input) {
+	void getProducts(std::vector<Package>& input) {
 		for (Package package : input) {
-			shelf[package.getNumber()].addCount(package.getCount());
+			shelf_[package.getNumber()].addCount(package.getCount());
 		}
 	}
 
-	std::vector<int> GetOrderList(std::vector<std::pair<Package, int>>& prices) {
+	std::vector<int> getOrderList(std::vector<std::pair<Package, int>>& prices) {
 		std::vector <int> order;
 		for (auto& package : prices) {
 			int number = package.first.getNumber();
 			if (package.first.getPrice() == package.second) {
-				order.push_back(std::max(0.0, max_count[number] * 0.8 - shelf[number].getCount()));
+				order.push_back(std::max(0.0, max_count_[number] * 0.8 - shelf_[number].getCount()));
 			}
 			else {
-				order.push_back(max_count[number] - shelf[number].getCount());
+				order.push_back(max_count_[number] - shelf_[number].getCount());
 			}
 		}
 		return order;
 	}
 
-	void PerformDay() {
-		for (Package& package : shelf) {
-			package.setCount(std::max(0.0, package.getCount() - customer_count * package.getInterest()));
+	void performDay() {
+		for (Package& package : shelf_) {
+			package.setCount(std::max(0.0, package.getCount() - customer_count_ * package.getInterest()));
 		}
 	}
 
 private:
-	int customer_count;
-	std::vector<Package> shelf;
-	std::vector<int> max_count;
+	int customer_count_;
+	std::vector<Package> shelf_;
+	std::vector<int> max_count_;
 };
 
 class Provider {
@@ -125,7 +125,7 @@ class Provider {
 	~Provider() = default;
 
 private:
-	int day;
+	int day_;
 };
 
 int main() {
