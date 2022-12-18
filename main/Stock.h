1c89c919aed72d37580eba2b3b64c465ce8f9ac6
Alina Mansurova <TheDolp@outlook.com>
2022-12-18 17:41:23 +0300

    Начало склада

--- main/Stock.h
+++ main/Stock.h
@@ -1,6 +1,14 @@
 #pragma once
+#include "Market.h"
 
 class Stock {
+public:
+
+private:
+	int balance_;
+	std::vector <Market*> markets_;
+	std::vector <std::vector<Package>> orders_;
+	std::vector <std::vector<Package>> case_;
 
 };
 
