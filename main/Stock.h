aa5291426ad2eb50334af6a2d89f8dfdac70d26f
Иван Белецкий <iabeletsky@gmail.com>
2022-12-18 17:44:29 +0300

    fix deque

--- main/Stock.h
+++ main/Stock.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "Market.h"
+#include <deque>
 
 class Stock {
 public:
+	
 
 private:
 	int balance_;
 	std::vector <Market*> markets_;
 	std::vector <std::vector<Package>> orders_;
-	std::vector <std::vector<Package>> case_;
+	std::vector <std::deque<Package>> case_;
 
 };
-
