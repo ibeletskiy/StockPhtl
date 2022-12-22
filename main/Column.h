0c5e931a01112cc9284aa14643b80f570cc7cb1c
Иван Белецкий <iabeletsky@gmail.com>
2022-12-22 14:19:36 +0300

    to files

--- main/Column.h
+++ main/Column.h
@@ -7,59 +7,15 @@ using namespace sf;
 class Column {
 public:
 	Column() = default;
-	Column(Vector2f size, Vector2f pos, int value = 0, Color c = Color::Green, Vector2f panel_size = { 60, 25 }) : value_(value) {
-		shape_.setSize(size);
-		shape_.setSize(size);
-		panel_.setSize(panel_size);
-		panel_.setFillColor(Color::White);
-		panel_.setOutlineColor(Color::Black);
-		panel_.setOutlineThickness(1);
-		shape_.setFillColor(c);
-		text_.setFillColor(Color::Black);
-		text_.setCharacterSize(panel_size.y - 4);
-	}
+	Column(Vector2f size, Vector2f pos, int value = 0, Color c = Color::Green, Vector2f panel_size = { 60, 25 });
 
-	void setPosition(Vector2f pos) {
-		shape_.setPosition(Vector2f(pos.x, pos.y - shape_.getSize().y));
-	}
+	void setPosition(Vector2f pos);
+	void setValue(float val, int real);
+	void setSize(float size);
+	int getValue();
+	bool contains(Vector2i mouse);
+	void draw(RenderWindow& window, Vector2i mouse);
 
-	void setValue(float val, int real) {
-		shape_.setPosition(Vector2f(shape_.getPosition().x, shape_.getPosition().y + shape_.getSize().y - val));
-		shape_.setSize(Vector2f(shape_.getSize().x, val));
-		value_ = real;
-	}
-
-	void setSize(float size) {
-		shape_.setSize(Vector2f(size, shape_.getSize().y));
-	}
-
-	void draw(RenderWindow& window, Vector2i mouse) {
-		window.draw(shape_);
-		if (contains(mouse)) {
-			text_.setString(std::to_string(value_));
-			panel_.setSize(Vector2f(text_.getGlobalBounds().width + 10, panel_.getSize().y));
-			panel_.setPosition({ mouse.x - panel_.getSize().x, mouse.y - panel_.getSize().y});
-			text_.setPosition(Vector2f(panel_.getPosition().x + 5, panel_.getPosition().y ));
-			window.draw(panel_);
-			text_.setFont(font);
-			window.draw(text_);
-		}
-	}
-
-	int getValue() {
-		return value_;
-	}
-
-	bool contains(Vector2i mouse) {
-		return ((mouse.x >= shape_.getPosition().x &&
-			mouse.x <= (shape_.getPosition().x + shape_.getSize().x) &&
-			mouse.y >= shape_.getPosition().y &&
-			mouse.y <= (shape_.getPosition().y + shape_.getSize().y))) || 
-			((mouse.x >= shape_.getPosition().x &&
-				mouse.x <= (shape_.getPosition().x + shape_.getSize().x) &&
-				mouse.y <= shape_.getPosition().y &&
-				mouse.y >= (shape_.getPosition().y + shape_.getSize().y)));
-	}
 protected:
 	int value_;
 	RectangleShape shape_;
--- main/Statistic.h
+++ main/Statistic.h
@@ -8,54 +8,9 @@
 class Statistic {
 public:
 	Statistic() = default;
-	Statistic(Vector2f size, Vector2f position, int count = 0) : size_(size), position_(position) {
-		stats_.resize(count, Column({ 0, 0 }, { 0, 0 }, 0));
-		pointer_ = 0;
-		for (int i = 0; i < count; ++i) {
-			stats_[i].setPosition(Vector2f(position_.x + ((double)size_.x / stats_.size() + 1) * i, position_.y + size_.y));
-			stats_[i].setSize((double)size_.x / stats_.size());
-		}
-		zero_.setFillColor(Color::Black);
-		zero_.setString("0");
-		zero_.setCharacterSize(20);
-		zero_.setPosition(Vector2f(position.x - 30, position.y + size.y - 20 ));
-		max_text_.setFillColor(Color::Black);
-		max_text_.setCharacterSize(20);
-		max_text_.setPosition(Vector2f(position.x - 30, position.y ));
-	}
-	
-	void addValue(int value) {
-		if (std::abs(value) > max_) {
-			max_ = std::abs(value);
-			max_text_.setString(std::to_string(max_));
-			max_text_.setFont(font);
-			int value = max_text_.getLocalBounds().width;
-			max_text_.setPosition(Vector2f(position_.x - max_text_.getGlobalBounds().width - 10, position_.y));
-			for (int i = 0; i < stats_.size(); ++i) {
-				stats_[i].setValue(((double)stats_[i].getValue() / max_) * size_.y, stats_[i].getValue());
-			}
-		}
-		if (pointer_ != stats_.size()) {
-			stats_[pointer_++].setValue(((double)value / max_) * size_.y, value);
-		} else {
-			stats_.push_back(Column(Vector2f(0, ((double)value / max_) * size_.y), position_, value));
-			for (int i = 0; i < stats_.size(); ++i) {
-				stats_[i].setPosition(Vector2f(position_.x + ((double)size_.x / stats_.size() + 1) * i, position_.y + size_.y));
-				stats_[i].setSize((double)size_.x / stats_.size());
-			}
-			++pointer_;
-		}
-	}
-
-	void draw(RenderWindow& window, Vector2i mouse) {
-		for (int i = 0; i < stats_.size(); ++i) {
-			stats_[i].draw(window, mouse);
-		}
-		zero_.setFont(font);
-		max_text_.setFont(font);
-		window.draw(zero_);
-		window.draw(max_text_);
-	}
+	Statistic(Vector2f size, Vector2f position, int count = 0);
+	void addValue(int value);
+	void draw(RenderWindow& window, Vector2i mouse);
 
 private:
 	std::vector<Column> stats_;
