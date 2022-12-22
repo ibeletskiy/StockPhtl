5716275901ebb2d9c78b0db2b79d469a6db1fbbc
Alina Mansurova <TheDolp@outlook.com>
2022-12-22 10:48:06 +0300

    split

--- main/Provider.cpp
+++ main/Provider.cpp
@@ -1,1 +1,48 @@
 #include "Provider.h"
+
+	Provider::Provider(std::vector<Package>& basic_shelf) {
+		for (auto package : basic_shelf) {
+			volume_.push_back(package.getCount());
+		}
+		day_ = 0;
+	}
+
+	void Provider::performDay() {
+		order_.clear();
+		++day_;
+		for (int i = 0; i < item_queue_.size(); ++i) {
+			int date;
+			if (!item_queue_[i].empty()) {
+				date = day_ + item_queue_[i].front().getDuration();
+			}
+			while (!item_queue_[i].empty() && item_queue_[i].front().getLastDay() == day_) {
+				order_.push_back(item_queue_[i].front());
+				order_.back().setLastDay(date);
+				item_queue_[i].pop();
+			}
+		}
+	}
+
+	std::vector<Package> Provider::sendDelivery() {
+		return order_;
+	}
+
+	void Provider::getOrder(std::vector<Package>& order) {
+		for (int i = 0; i < order.size(); ++i) {
+			if (order[i].getCount() == 0) continue;
+			int date = day_;
+			if (item_queue_[i].empty()) {
+				date += 1;
+			}
+			else {
+				date = item_queue_[i].back().getLastDay() + 1;
+			}
+			Package package = order[i];
+			package.setCount(volume_[i]);
+			package.setLastDay(date);
+			for (int j = 0; j < order[i].getCount(); ++j) {
+				item_queue_[i].push(package);
+			}
+		}
+	}
+
