0ab5abd8b9a1c77cd475d97045c5b1409709441a
Иван Белецкий <iabeletsky@gmail.com>
2022-12-22 15:14:04 +0300

    do something

--- main/Plane.h
+++ main/Plane.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "Provider.h"
+#include "Market.h"
+#include "Button.h"
+#include "Stock.h"
+#include "Statistic.h"
+#include <vector>
 
 class Plane {
+public:
+	Plane();
+	
+	void play();
 
+private:
+	Provider provider_;
+	Stock stock_;
+	Manager* manager_;
+	std::vector<Market> markets_;
+	std::vector<Button> market_buttons_;
+	std::vector<Button> shelves_;
+	Statistic stats_;
 };
