7d19774affa1bd1045a3ee8dcbe98c6ec959a47e
Alina Mansurova <TheDolp@outlook.com>
2022-12-22 12:09:14 +0300

    опа

--- main/Stock.cpp
+++ main/Stock.cpp
@@ -1,1 +1,72 @@
 #include "Stock.h"
+
+Stock::Stock(std::vector<Market*> markets, std::vector<Package>& basic_shelf, std::vector<int>& max_count, Provider* provider,
+	Manager* manager) :
+	balance_(0), day_(0), markets_(markets), items_(basic_shelf), max_count_(max_count), provider_(provider), manager_(manager) {
+	std::sort(markets_.begin(), markets_.end(), [](Market* a, Market* b) { return *b < *a; });
+	orders_.resize(markets.size());
+	for (auto& vec : orders_) {
+		vec.resize(items_.size(), 0);
+	}
+	ordered_.resize(items_.size(), 0);
+}
+
+void Stock::getDelivery() {
+	std::vector <Package> order = provider_->sendDelivery();
+	for (Package package : order) {
+		case_[package.getNumber()].push_back(package);
+		ordered_[package.getNumber()]--;
+	}
+}
+
+void Stock::makeOrder() {
+	std::vector <Package> order;
+	for (int i = 0; i < items_.size(); ++i) {
+		order[i].setCount(max_count_[i] - case_[i].size());
+	}
+	manager_->makeOrder(order);
+	provider_->getOrder(order);
+	for (int i = 0; i < items_.size(); ++i) {
+		ordered_[i] += order[i].getCount();
+	}
+}
+
+void Stock::makePrices() {
+	manager_->makePrices(items_);
+}
+
+void Stock::getOrder() {
+	for (int i = 0; i < markets_.size(); ++i) {
+		std::vector <int> order = markets_[i]->getOrderList(items_);
+		for (int j = 0; j < order.size(); ++j) {
+			orders_[i][j] += order[j];
+		}
+	}
+}
+
+void Stock::writeOff() {
+	for (auto& v : case_) {
+		while (!v.empty() && v[0].getLastDay() < day_) {
+			// возможно положить посчитать количество мусора
+			v.pop_front();
+		}
+	}
+}
+
+void Stock::sendDelivery() {
+	for (int i = 0; i < markets_.size(); ++i) {
+		std::vector<Package> to_send;
+		for (int j = 0; j < orders_.size(); ++j) {
+			while (!case_[j].empty() && case_[j][0].getCount() <= orders_[i][j]) {
+				balance_ += case_[j][0].getActual();
+				to_send.push_back(case_[j][0]);
+				case_[j].pop_front();
+			}
+		}
+		markets_[i]->getProducts(to_send);
+	}
+}
+
+void Stock::setManager(Manager* manager) {
+	manager_ = manager;
+}
--- main/Stock.h
+++ main/Stock.h
@@ -9,76 +9,21 @@
 class Stock {
 public:
 
-	Stock(std::vector<Market*> markets, std::vector<Package>& basic_shelf, std::vector<int>& max_count, Provider* provider,
-		Manager* manager):
-		balance_(0), day_(0), markets_(markets), items_(basic_shelf), max_count_(max_count), provider_(provider), manager_(manager) {
-		std::sort(markets_.begin(), markets_.end(), [](Market* a, Market* b) { return *b < *a; });
-		orders_.resize(markets.size());
-		for (auto& vec : orders_) {
-			vec.resize(items_.size(), 0);
-		}
-		ordered_.resize(items_.size(), 0);
-	}
+	Stock(std::vector<Market*> markets, std::vector<Package>& basic_shelf, std::vector<int>& max_count, Provider* provider, Manager* manager);
 
-	void getDelivery() {
-		std::vector <Package> order = provider_->sendDelivery();
-		for (Package package : order) {
-			case_[package.getNumber()].push_back(package);
-			ordered_[package.getNumber()]--;
-		}
-	}
+	void getDelivery();
 
-	void makeOrder() {
-		std::vector <Package> order;
-		for (int i = 0; i < items_.size(); ++i) {
-			order[i].setCount(max_count_[i] - case_[i].size());
-		}
-		manager_->makeOrder(order);
-		provider_->getOrder(order);
-		for (int i = 0; i < items_.size(); ++i) {
-			ordered_[i] += order[i].getCount();
-		}
-	}
+	void makeOrder();
 
-	void makePrices() {
-		manager_->makePrices(items_);
-	}
+	void makePrices();
 
-	void getOrder() {
-		for (int i = 0; i < markets_.size(); ++i) {
-			std::vector <int> order = markets_[i]->getOrderList(items_);
-			for (int j = 0; j < order.size(); ++j) {
-				orders_[i][j] += order[j]; 
-			}
-		}
-	}
+	void getOrder();
 
-	void writeOff() {
-		for (auto& v : case_) {
-			while (!v.empty() && v[0].getLastDay() < day_) {
-				// возможно положить посчитать количество мусора
-				v.pop_front();
-			}
-		}
-	}
+	void writeOff();
 
-	void sendDelivery() {
-		for (int i = 0; i < markets_.size(); ++i) {
-			std::vector<Package> to_send;
-			for (int j = 0; j < orders_.size(); ++j) {
-				while (!case_[j].empty() && case_[j][0].getCount() <= orders_[i][j]) {
-					balance_ += case_[j][0].getActual();
-					to_send.push_back(case_[j][0]);
-					case_[j].pop_front();
-				}
-			}
-			markets_[i]->getProducts(to_send);
-		}
-	}
+	void sendDelivery();
 
-	void setManager(Manager* manager) {
-		manager_ = manager;
-	}
+	void setManager(Manager* manager);
 
 	friend void Smart::makeOrder(std::vector <Package>& items);
 	friend void Smart::makePrices(std::vector <Package>& items);
